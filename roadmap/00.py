ffef93a6e2ddc6ef373a0dfebc505e55bdb9277d
SujetoJR <jorgerissiotti2@gmail.com>
2026-04-19 08:50:26 -0400

    feat: Add roadmap exercice 00 and add files for the rest of the roadmap exercises

--- roadmap/00.py
+++ roadmap/00.py
@@ -1,2 +1,26 @@
-# syntax, variables, data types and hello python
+# 00 syntax, variables, data types and hello python
 
+# https://www.python.org/
+
+'''
+This is a multi-line comment
+with single quotes
+'''
+
+"""
+This is a multi-line comment
+with double quotes
+"""
+
+my_variable = "This is a variable"
+my_variable = "This is the previous variable with other value"
+
+MY_CONSTANT = "This is a constant variable" #by convention
+
+my_int = 19
+my_float = 4.5
+my_string = "Python"
+my_bool = True
+my_bool = False
+
+print(f"Hello {my_string}")
